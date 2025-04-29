2d26f445ac128f882014316126c8b539342c8679
fndeveloper <fasihnasir60@gmail.com>
2025-04-30 03:04:02 +0500

    :

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom' // 👈 react-router-dom se import karo
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 // =================== COMPONENTS START ===========================
 import Navbar from './component/navbar'
-// =================== COMPONENTS END ===========================
-// =================== PAGE START ===========================
+// =================== COMPONENTS END =============================
+// =================== PAGE START ================================
 import Home from '../page/home'
-// =================== PAGE START ===========================
-
+// =================== PAGE END ===================================
 
 function App() {
   const [count, setCount] = useState(0)
+
+  // Tawk.to Script Integration
+  useEffect(() => {
+    var Tawk_API = Tawk_API || {};
+    var Tawk_LoadStart = new Date();
+    (function () {
+      var s1 = document.createElement("script");
+      s1.async = true;
+      s1.src = "https://embed.tawk.to/68114b7009ac26190b4aa713/1iq1otc2m";
+      s1.charset = "UTF-8";
+      s1.setAttribute("crossorigin", "*");
+      var s0 = document.getElementsByTagName("script")[0];
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+  }, []);
 
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          {/* Yahan Route define karoge */}
-          <Route path="/" element={<Home/>} />
-          {/* aur agar chaho to aur bhi route add karo */}
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
